3220c6d4bb988cd6bac3fc6e2fd51144ae8a1859
nicholashudson2 <nicholas.hudson2@yahoo.com>
2017-09-03 09:03:52 -0500

    Incomplete. Still working.

--- Employee.js
+++ Employee.js
@@ -3,12 +3,34 @@
 const fs = require('fs')
 
 class Employee {
-  constructor (/* ??? */) {
-    // TODO
+  constructor() {
+
+    const Employee = function (name, title, salary){ //function (name, title, salary) 
+      this.name = name
+      this.title = title
+      this.salary = salary
+
+      this.promote = (newTitle, newSalary) => {
+        this.title = newTitle
+        this.salary = newSalary
+      }
+    
+    function parseFromFilePath (file, callback) {
+      fs.readFileSync(file, (err, data) => {
+        const fileData = JSON.parseSync(data)
+        this.name = fileData.name
+        this.title = fileData.title
+        this.salary = fileName.salary
+        callback
+      }
+      )
+    }
+  }
+    // TODO ???
   }
 
-  // TODO ???
 }
+
 
 // TODO ???
 
